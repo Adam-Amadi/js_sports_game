fcb553d600f0e5a17beb932a3eb5f8b1043d4ff7
ADAM AMADI <ADAM.AMADI22@GMAIL.COM>
2020-05-01 17:35:05 -0500

    adding codes

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+
+// * Team one declarations *//
+const teamOneShoot = document.querySelector("#teamone-shoot-button");
+const teamOneShots = document.querySelector("#teamone-numshots")
+const teamOneGoals = document.querySelector("#teamone-numgoals")
+
+
+
+// * Team two declarations * //
+const teamTwoShoot = document.querySelector("#teamtwo-shoot-button");
+const teamOneShots = document.querySelector("#teamtwo-numshots")
+const teamOneGoals = document.querySelector("#teamtwo-numgoals")
